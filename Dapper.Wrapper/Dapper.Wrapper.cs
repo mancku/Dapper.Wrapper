b73d3d5d8c88a0e6d7acdb9ad9e1d006887868c5
mancku <adria.lopez.ortega@gmail.com>
2024-03-28 12:54:49 +0100

    Implementing IDisposable

--- Dapper.Wrapper/Dapper.Wrapper.cs
+++ Dapper.Wrapper/Dapper.Wrapper.cs
@@ -8,7 +8,7 @@
     using System.Threading.Tasks;
     using Microsoft.Extensions.Configuration;
 
-    public partial class DapperWrapper
+    public partial class DapperWrapper : IDisposable
     {
         /// <summary>
         /// 
