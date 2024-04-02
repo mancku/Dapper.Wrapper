05395c7e38bf03c74f006ecc537f1c9e36c1d84d
Adrià López <adria.lopez.ortega@gmail.com>
2024-04-02 18:11:42 +0200

    Added TestsCollection and BaseTest

---
+++ Dapper.Wrapper.Test/BaseDapperWrapperTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Dapper.Wrapper.Test
+{
+    using Bogus;
+    using System;
+
+    [Collection("DapperWrapperTestsCollection")]
+    public abstract class BaseDapperWrapperTests : IDisposable
+    {
+        protected DapperWrapper _msSqlDapperWrapper;
+        protected DapperWrapper _postgreSqlDapperWrapper;
+        protected DapperWrapper _mySqlDapperWrapper;
+
+        private readonly TestContainersHandlerFixture _fixture;
+        protected readonly Faker Faker = new();
+
+
+        protected BaseDapperWrapperTests(TestContainersHandlerFixture fixture)
+        {
+            _fixture = fixture;
+            _msSqlDapperWrapper = new DapperWrapper(_fixture.MsSqlConnectionString, FastCrud.SqlDialect.MsSql);
+            _postgreSqlDapperWrapper = new DapperWrapper(_fixture.PostgreSqlConnectionString, FastCrud.SqlDialect.PostgreSql);
+            _mySqlDapperWrapper = new DapperWrapper(_fixture.MySqlConnectionString, FastCrud.SqlDialect.MySql);
+        }
+
+        public void Dispose()
+        {
+            _msSqlDapperWrapper.Dispose();
+            _postgreSqlDapperWrapper.Dispose();
+            _mySqlDapperWrapper.Dispose();
+        }
+    }
+}
