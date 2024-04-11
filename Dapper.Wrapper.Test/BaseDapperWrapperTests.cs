5392298772ebf4fe0e91bfce07969357e7ffe2cf
mancku <adria.lopez.ortega@gmail.com>
2024-04-11 19:47:24 +0200

    Adding SqlManager to TestBase

--- Dapper.Wrapper.Test/BaseDapperWrapperTests.cs
+++ Dapper.Wrapper.Test/BaseDapperWrapperTests.cs
@@ -2,25 +2,36 @@
 {
     using Bogus;
     using FastCrud;
-    using System;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
 
     [Collection("DapperWrapperTestsCollection")]
     public abstract class BaseDapperWrapperTests : IDisposable
     {
         protected readonly Faker Faker = new();
-        
-        private readonly DapperWrapper _msSqlDapperWrapper;
-        private readonly DapperWrapper _postgreSqlDapperWrapper;
-        private readonly DapperWrapper _mySqlDapperWrapper;
 
         private readonly TestContainersHandlerFixture _fixture;
+        private readonly DapperWrapper _msSqlDapperWrapper;
+        private readonly DapperWrapper _mySqlDapperWrapper;
+        private readonly DapperWrapper _postgreSqlDapperWrapper;
+
+        private readonly DapperWrapperSqlManager _msSqlDapperWrapperSqlManager;
+        private readonly DapperWrapperSqlManager _mySqlDapperWrapperSqlManager;
+        private readonly DapperWrapperSqlManager _postgreSqlDapperWrapperSqlManager;
 
         protected BaseDapperWrapperTests(TestContainersHandlerFixture fixture)
         {
             _fixture = fixture;
-            _msSqlDapperWrapper = new DapperWrapper(_fixture.MsSqlConnectionString, FastCrud.SqlDialect.MsSql);
-            _postgreSqlDapperWrapper = new DapperWrapper(_fixture.PostgreSqlConnectionString, FastCrud.SqlDialect.PostgreSql);
-            _mySqlDapperWrapper = new DapperWrapper(_fixture.MySqlConnectionString, FastCrud.SqlDialect.MySql);
+            _msSqlDapperWrapper = new DapperWrapper(_fixture.MsSqlConnectionString, SqlDialect.MsSql);
+            _postgreSqlDapperWrapper = new DapperWrapper(_fixture.PostgreSqlConnectionString, SqlDialect.PostgreSql);
+            _mySqlDapperWrapper = new DapperWrapper(_fixture.MySqlConnectionString, SqlDialect.MySql);
+
+            var configuration = fixture._configuration;
+            var logger = LoggerFactory.Create(builder => { }).CreateLogger("Test");
+
+            _msSqlDapperWrapperSqlManager = new DapperWrapperSqlManager(configuration, logger, _fixture.MsSqlConnectionString, SqlDialect.MsSql);
+            _postgreSqlDapperWrapperSqlManager = new DapperWrapperSqlManager(configuration, logger, _fixture.PostgreSqlConnectionString, SqlDialect.PostgreSql);
+            _mySqlDapperWrapperSqlManager = new DapperWrapperSqlManager(configuration, logger, _fixture.MySqlConnectionString, SqlDialect.MySql);
         }
 
         public void Dispose()
@@ -37,6 +48,17 @@
                 SqlDialect.MsSql => _msSqlDapperWrapper,
                 SqlDialect.MySql => _mySqlDapperWrapper,
                 SqlDialect.PostgreSql => _postgreSqlDapperWrapper,
+                _ => throw new ArgumentOutOfRangeException(nameof(sqlDialect), sqlDialect, null)
+            };
+        }
+
+        protected DapperWrapperSqlManager GetSqlManager(SqlDialect sqlDialect)
+        {
+            return sqlDialect switch
+            {
+                SqlDialect.MsSql => _msSqlDapperWrapperSqlManager,
+                SqlDialect.MySql => _mySqlDapperWrapperSqlManager,
+                SqlDialect.PostgreSql => _postgreSqlDapperWrapperSqlManager,
                 _ => throw new ArgumentOutOfRangeException(nameof(sqlDialect), sqlDialect, null)
             };
         }
